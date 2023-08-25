46d5d6bfe1fbb9725a837cba1bd967c0afa03dad
Fiqri Syah Redha <wibulo27@gmail.com>
2023-08-26 00:57:44 +0700

    setup tickets service

---
+++ apps/tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,129 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+import { natsWrapper } from "../../__mocks__/nats-wrapper";
+
+it("returns a 404 if the provided id is not exist", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "jofjewu",
+      price: 30
+    })
+    .expect(404);
+});
+
+it("returns a 401 if the user is not authenticated", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({
+      title: "jofjewu",
+      price: 30
+    })
+    .expect(401);
+});
+
+it("returns a 401 if the user does not own the ticke", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "dasjf9u3r",
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "9120u93difsa",
+      price: 50
+    })
+    .expect(401);
+});
+
+it("returns a 400 if the user provided an invalid title or price", async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "dasjf9u3r",
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "",
+      price: 30
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "jfs9udas",
+      price: -10
+    })
+    .expect(400);
+});
+
+it("updates the ticket provided with valid inputs", async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "dasjf9u3r",
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "new title",
+      price: 30
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual("new title");
+  expect(ticketResponse.body.price).toEqual(30);
+});
+
+it("publishes an event", async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "dasjf9u3r",
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "new title",
+      price: 30
+    })
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
